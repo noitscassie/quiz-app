7f46c6c51ee4b88c3433c26846845ebcc13684a6
Peter Johnstone <peter.wdj@gmail.com>
2018-01-08 15:58:35 +0000

    Refactor to use let instead of before block

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,17 +1,17 @@
 require('round.rb')
 
 describe Round do
+  let :subject { Round.new }
+
   describe '#question' do
     it 'Should have a question' do
-      round = Round.new
-      expect(round.question).to be_truthy
+      expect(subject.question).to be_truthy
     end
   end
 
   describe '#answer' do
     it 'Should have an answer' do
-      round = Round.new
-      expect(round.answer).to be_truthy
+      expect(subject.answer).to be_truthy
     end
   end
 end
